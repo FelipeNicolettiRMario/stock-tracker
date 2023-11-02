99390d2020f7d33f3bf0d2726fd19e2792abfd83
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2023-11-02 16:55:06 -0300

    add(services): Adding interfaces declaration + rule validations implementation

--- app/stock_tracker/services/client.py
+++ app/stock_tracker/services/client.py
@@ -1,7 +1,15 @@
 from stock_tracker.models.client import Client
 from stock_tracker.repositories.client_repository import IClientRepository
 
-class ClientService:
+from abc import ABC, abstractmethod
+
+class IClientService(ABC):
+
+    @abstractmethod
+    def add_client(self, client_data: dict):
+        pass
+
+class ClientService(IClientService):
 
     def __init__(self, repo: IClientRepository) -> None:
         self.__repo = repo
--- app/stock_tracker/services/rule.py
+++ app/stock_tracker/services/rule.py
@@ -2,7 +2,21 @@ from stock_tracker.repositories.rule_repository import IRuleRepository
 from stock_tracker.models.rule import Rule
 
 from typing import List
+from abc import ABC, abstractmethod
 
+class IRuleService(ABC):
+    
+    @abstractmethod
+    def save_rule(self, rule_data: dict):
+        pass
+
+    @abstractmethod
+    def get_all_rules(self) -> List[Rule]:
+        pass
+
+    @abstractmethod
+    def is_rule_satisfied(self, rule:Rule) -> bool:
+        pass
 
 class RuleService:
 
@@ -16,3 +30,29 @@ class RuleService:
     def get_all_rules(self) -> List[Rule]:
         self.__repo.get_all_rules()
     
+    def _is_targe_operation_rule_satisfied(self, rule: Rule) -> bool:
+
+        ticker_actual_value = self.__repo.get_rule_ticker_actual_value(rule.ticker)
+
+        if rule.operator:
+            return ticker_actual_value > rule.target
+        
+        else:
+            return ticker_actual_value < rule.target
+
+    def _is_comparison_operation_rule_satisfied(self, rule: Rule) -> bool:
+
+        comparision_target_actual_value = self.__repo.get_rule_ticker_actual_value(rule.comparison_target)
+        ticker_actual_value = self.__repo.get_rule_ticker_actual_value(rule.ticker)
+
+        if rule.operator:
+            return ticker_actual_value > comparision_target_actual_value
+        else:
+            return ticker_actual_value < comparision_target_actual_value
+
+    def is_rule_satisfied(self, rule:Rule) -> bool:
+
+        if not rule.comparison_target:
+            return self._is_targe_operation_rule_satisfied(rule)
+        
+        return self._is_comparison_operation_rule_satisfied(rule)
